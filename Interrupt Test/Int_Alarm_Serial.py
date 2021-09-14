b95300a418e145c07225d645ce19091e2631c1d8
mateusmassa <mateusmassa7@gmail.com>
2021-09-14 14:55:43 -0300

    Update code

--- Interrupt Test/Int_Alarm_Serial.py
+++ Interrupt Test/Int_Alarm_Serial.py
@@ -4,41 +4,17 @@ from time import sleep
 
 # ----- Setup the Serial COM Port ------
 
-ser0 = serial.Serial(
-port='/dev/ttyUSB0',
-baudrate = 9600,
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-timeout=1
-)
+ser0 = serial.Serial( port='/dev/ttyUSB0', baudrate = 9600, parity=serial.PARITY_NONE,
+                      stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-ser1 = serial.Serial(
-port='/dev/ttyUSB1',
-baudrate = 9600,
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-timeout=1
-)
+ser1 = serial.Serial( port='/dev/ttyUSB1', baudrate = 9600, parity=serial.PARITY_NONE,
+                      stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-ser2 = serial.Serial(
-port='/dev/ttyUSB2',
-baudrate = 9600,
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-timeout=1
-)
+ser2 = serial.Serial( port='/dev/ttyUSB2', baudrate = 9600, parity=serial.PARITY_NONE, 
+                      stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-ser3 = serial.Serial(
-port='/dev/ttyUSB3',
-baudrate = 9600,
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-timeout=1
-)
+ser3 = serial.Serial( port='/dev/ttyUSB3', baudrate = 9600, parity=serial.PARITY_NONE, 
+                      stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
 def handler(signum, frame):
     print "Interrupcao! - Leitura de Sensores"
